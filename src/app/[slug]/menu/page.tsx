dfddd840538640a543ed484c4304fc6ad6e5b478
Vitor-Kemuel <kemuvitor@gmail.com>
2025-02-23 20:00:23 -0300

    feat: aula 4

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -6,40 +6,40 @@ import RestaurantCategories from "./components/categories";
 import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
-  params: Promise<{ slug: string }>;
-  searchParams: Promise<{ consumptionMethod: string }>;
+    params: Promise<{ slug: string }>;
+    searchParams: Promise<{ consumptionMethod: string }>;
 }
 
 const isConsumptionMethodValid = (consumptionMethod: string) => {
-  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+    return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
 };
 
 const RestaurantMenuPage = async ({
-  params,
-  searchParams,
+    params,
+    searchParams,
 }: RestaurantMenuPageProps) => {
-  const { slug } = await params;
-  const { consumptionMethod } = await searchParams;
-  if (!isConsumptionMethodValid(consumptionMethod)) {
-    return notFound();
-  }
-  const restaurant = await db.restaurant.findUnique({
-    where: { slug },
-    include: {
-      menuCategories: {
-        include: { products: true },
-      },
-    },
-  });
-  if (!restaurant) {
-    return notFound();
-  }
-  return (
-    <div>
-      <RestaurantHeader restaurant={restaurant} />
-      <RestaurantCategories restaurant={restaurant} />
-    </div>
-  );
+    const { slug } = await params;
+    const { consumptionMethod } = await searchParams;
+    if (!isConsumptionMethodValid(consumptionMethod)) {
+        return notFound();
+    }
+    const restaurant = await db.restaurant.findUnique({
+        where: { slug },
+        include: {
+            menuCategories: {
+                include: { products: true },
+            },
+        },
+    });
+    if (!restaurant) {
+        return notFound();
+    }
+    return (
+        <div>
+            <RestaurantHeader restaurant={restaurant} />
+            <RestaurantCategories restaurant={restaurant} />
+        </div>
+    );
 };
 
 export default RestaurantMenuPage;
